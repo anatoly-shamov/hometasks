f5d4b64851b5c9c3a36af0b0547fe32863c88748
Anatoly Shamov <anatoly.shamov@gmail.com>
2018-02-12 16:17:11 +0300

    Curve25519SignatureForger complete

--- src/main/scala/crypto/Curve25519SignatureForger.scala
+++ src/main/scala/crypto/Curve25519SignatureForger.scala
@@ -4,6 +4,15 @@ import scorex.crypto.signatures.Signature
 
 object Curve25519SignatureForger {
 
-  def forgeSignature(signature: Signature): Signature = ???
+  def forgeSignature(signature: Signature): Signature = {
+    // l parameter for Ed25519-SHA-512
+    // see https://ed25519.cr.yp.to/ed25519-20110926.pdf, https://eprint.iacr.org/2015/677.pdf
+    val l = BigInt(2).pow(252) + BigInt("27742317777372353535851937790883648493")
+
+    Signature(
+      signature.take(32) ++ //R
+        (BigInt(signature.takeRight(32).reverse) + l).toByteArray.reverse //S + l
+    )
+  }
 
 }
